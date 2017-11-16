a79a9e7c7f39ad1d5fef4177986d919ee11be5f7
Max han <mwellh84@gmail.com>
2017-11-16 04:53:24 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,18 @@
 'use strict';
+
+class Question{
+  constructor(){
+    this.save()
+  }
+  save(){
+    this.constructor._All.push(this)
+  }
+  static All(){
+    return this._All;
+  }
+}
+
+Question._All = []
+
+const question = new Question("What's your favorite food?")
+question.save()
